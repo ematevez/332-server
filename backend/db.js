98bc827a2a3bdf46362f51c173e720ac6ba15180
Ematevez <ematevez@gmail.com>
2025-06-10 20:43:16 -0300

    first commit

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://user:user@cluster0.5cff1qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("✔ Conectado a MongoDB Atlas");
+  } catch (err) {
+    console.error("❌ Error de conexión", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
