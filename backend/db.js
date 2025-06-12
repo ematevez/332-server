3de037ba05762254346b49b0c05144fe4a7d5713
Ematevez <ematevez@gmail.com>
2025-06-11 21:12:16 -0300

    cambio de ruta anibal

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://user:user@cluster0.5cff1qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    // await mongoose.connect("mongodb+srv://user:user@cluster0.5cff1qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://anibalcaeiro:WKAVCJwJheqziPgN@cluster0.g4hk2z4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    
     console.log("✔ Conectado a MongoDB Atlas");
   } catch (err) {
     console.error("❌ Error de conexión", err);
