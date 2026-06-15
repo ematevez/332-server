1a54664e69c4eb0fd5a0d6c37ecf95f9cc1e944a
Emanuel Tevez <70983593+ematevez@users.noreply.github.com>
2026-06-15 14:52:59 -0300

    Update MongoDB connection string and error handling

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect("mongodb+srv://user:user@cluster0.5cff1qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-    // await mongoose.connect("mongodb+srv://anibalcaeiro:WKAVCJwJheqziPgN@cluster0.g4hk2z4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-    await mongoose.connect("mongodb+srv://user:user@cluster0.zirxpbn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-
-    
-    console.log("✔ Conectado a MongoDB Atlas");
-  } catch (err) {
-    console.error("❌ Error de conexión", err);
+    const conn = await mongoose.connect(
+      'mongodb+srv://user:user@lectura.k0ihs9e.mongodb.net/qwent?retryWrites=true&w=majority',
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    console.log(`MongoDB Conectado: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
