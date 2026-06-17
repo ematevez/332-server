b43ad44e87d2383098d93bd87e06cea21f39c9ad
ematevez <ematevez@gmail.com>
2026-06-16 22:53:59 -0300

    External api4

--- frontend/src/ExternalUsers.js
+++ frontend/src/ExternalUsers.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; // Reutilizamos los estilos
 
-const API_URL = 'http://localhost:5000/api/external-users';
+const API_URL = 'https://devsapihub.com/api-users';
 
 function ExternalUsers() {
   const [users, setUsers] = useState([]);
